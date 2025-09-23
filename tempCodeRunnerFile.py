fce2f45e533fbaec4e303aaa9155d17376d8bd12
ElizabethNuomaAkossey <elizabethnuomaakossey@gmail.com>
2025-09-23 14:15:55 +0000

    deleted some files that were not needed

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,18 @@
-@ui.page('/vendor')
+ui.add_head_html("""
+<style>
+@keyframes typing-loop {
+  0% { width: 0; }
+  40% { width: 100%; }
+  90% { width: 100%; }
+  100% { width: 0; }
+}
+
+.typewriter {
+  display: inline-block;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 0;
+  animation: typing-loop 7s steps(12, end) infinite;
+}
+</style>
+""")
